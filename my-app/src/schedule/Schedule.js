2f26fed124af649b783e68a595da3054c0ece54a
zhangdora <zhangli@zhanglideMacBook-Pro.local>
2018-09-11 23:22:10 +0800

    add png files

--- my-app/src/schedule/Schedule.js
+++ my-app/src/schedule/Schedule.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './schedule.less';
+import scheduleLogo from "./schedule.png";
 
 class Schedule extends Component {
   render() {
     return (
       <div className="App">
+        <img src={scheduleLogo}/>
       </div>
     );
   }
